aac69208db42e11d3e2c0dbd494bc74cc32bcbf6
Gilberto Stankiewicz <hello@kiewic.com>
2018-08-16 19:18:27 -0700

    Add ClipboardCopyPaste project

---
+++ ClipboardCopyPaste/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipboardCopyPaste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunInSta(() =>
+            {
+                var currentData = Clipboard.GetDataObject();
+                var newData = new System.Windows.Forms.DataObject();
+
+                // Go through all the formats in the clipboard, and only copy
+                // the formats selected with CopyDataFromCurrentToNew().
+                // All other formats will be removed from the clipboard.
+                foreach (string format in currentData.GetFormats())
+                {
+                    // Sometimes CF_BITMAP show as a zero length content,
+                    // but Handle Type is bitmap, which apparently is treated differently.
+                    // Is there really an image in the clipboard?
+                    Console.WriteLine(format);
+                    if (format == "Bitmap")
+                    {
+                        var bitmap = currentData.GetData(format) as System.Drawing.Bitmap;
+                        Console.WriteLine("{0}x{1}", bitmap.Width, bitmap.Height);
+                    }
+
+                    //CopyDataFromCurrentToNew(currentData, newData, format, "CF_UNICODETEXT");
+                    CopyDataFromCurrentToNew(currentData, newData, format, "PNG");
+                }
+
+                Clipboard.SetDataObject(newData, true);
+            });
+        }
+
+        private static void CopyDataFromCurrentToNew(IDataObject currentData, DataObject newData, string currentFormat, string expectedFormat)
+        {
+            if (currentFormat == expectedFormat)
+            {
+                var formattedData = currentData.GetData(currentFormat);
+                newData.SetData(currentFormat, formattedData);
+            }
+        }
+
+        internal static void RunInSta(Action action)
+        {
+            Thread thread = new Thread(() => action());
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Windows Clipboard Copy & Paste Using C#
+
+Gets all the content from the Windows Clipboard, and creates a new IDataObject with only
+the selected formats, and puts them back into the clipboard.
+All other formats are removed from the clipboard.
+
+This is a good C# example of:
+
+* How to check what is inside the clipboard.
+* Retrieve content from the clipboard.
+* Add multiple formats at the same time into the clipboard.
