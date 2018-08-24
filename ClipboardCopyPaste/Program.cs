7a5948520c4b39579c2a0c5a14c879bf76880214
Gilberto Stankiewicz <hello@kiewic.com>
2018-08-24 16:02:23 -0700

    Use `System.STAThreadAttribute`

--- ClipboardCopyPaste/Program.cs
+++ ClipboardCopyPaste/Program.cs
@@ -10,34 +10,32 @@ namespace ClipboardCopyPaste
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            RunInSta(() =>
+            var currentData = Clipboard.GetDataObject();
+            var newData = new System.Windows.Forms.DataObject();
+
+            // Go through all the formats in the clipboard, and only copy
+            // the formats selected with CopyDataFromCurrentToNew().
+            // All other formats will be removed from the clipboard.
+            foreach (string format in currentData.GetFormats())
             {
-                var currentData = Clipboard.GetDataObject();
-                var newData = new System.Windows.Forms.DataObject();
-
-                // Go through all the formats in the clipboard, and only copy
-                // the formats selected with CopyDataFromCurrentToNew().
-                // All other formats will be removed from the clipboard.
-                foreach (string format in currentData.GetFormats())
+                // Sometimes CF_BITMAP show as a zero length content,
+                // but Handle Type is bitmap, which apparently is treated differently.
+                // Is there really an image in the clipboard?
+                Console.WriteLine(format);
+                if (format == "Bitmap")
                 {
-                    // Sometimes CF_BITMAP show as a zero length content,
-                    // but Handle Type is bitmap, which apparently is treated differently.
-                    // Is there really an image in the clipboard?
-                    Console.WriteLine(format);
-                    if (format == "Bitmap")
-                    {
-                        var bitmap = currentData.GetData(format) as System.Drawing.Bitmap;
-                        Console.WriteLine("{0}x{1}", bitmap.Width, bitmap.Height);
-                    }
-
-                    //CopyDataFromCurrentToNew(currentData, newData, format, "CF_UNICODETEXT");
-                    CopyDataFromCurrentToNew(currentData, newData, format, "PNG");
+                    var bitmap = currentData.GetData(format) as System.Drawing.Bitmap;
+                    Console.WriteLine("{0}x{1}", bitmap.Width, bitmap.Height);
                 }
 
-                Clipboard.SetDataObject(newData, true);
-            });
+                //CopyDataFromCurrentToNew(currentData, newData, format, "CF_UNICODETEXT");
+                CopyDataFromCurrentToNew(currentData, newData, format, "PNG");
+            }
+
+            Clipboard.SetDataObject(newData, true);
         }
 
         private static void CopyDataFromCurrentToNew(IDataObject currentData, DataObject newData, string currentFormat, string expectedFormat)
@@ -47,14 +45,6 @@ namespace ClipboardCopyPaste
                 var formattedData = currentData.GetData(currentFormat);
                 newData.SetData(currentFormat, formattedData);
             }
-        }
-
-        internal static void RunInSta(Action action)
-        {
-            Thread thread = new Thread(() => action());
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
-            thread.Join();
         }
     }
 }
